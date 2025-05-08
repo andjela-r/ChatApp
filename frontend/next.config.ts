73172b11b6910e6679a65ba7314414a9c38ebcce
Andjela Radojevic <a.radojevic01@gmail.com>
2025-05-08 16:23:11 +0200

    Fixed frontend reloading

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 1000,            // check for changes every second
-        aggregateTimeout: 300, // delay rebuild slightly to batch rapid changes
-        ignored: /node_modules/, // improve performance
-      };
+  /* config options here */
+  webpackDevMiddleware: (config: any) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
     }
-    return config;
-  },
+    return config
+  }
 };
 
 export default nextConfig;
